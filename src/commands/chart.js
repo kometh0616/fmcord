05cd0f3acdf1cdb07aa8d9fe17b84391b6745b9f
kometh0616 <realdr1v3n@gmail.com>
2018-12-28 01:14:27 +0200

    Limited y axis to 10 grids.

--- src/commands/chart.js
+++ src/commands/chart.js
@@ -38,6 +38,8 @@ exports.run = async (client, message, args) => {
   const [x, y] = axisArray;
   if (x > 5) return message.channel.send(`The first number of the grid size ` +
   `must not be bigger than 5 tiles!`);
+  if (y > 10) return message.channel.send(`The last number of the grid size ` +
+  `must not be bigger than 10 tiles!`);
 
   const { botOwnerID } = client.config;
   const Users = client.sequelize.import(`../models/Users.js`);
