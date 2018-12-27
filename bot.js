79f82437759792d319c33bc55f56443e2bdc8ad4
kometh0616 <realdr1v3n@gmail.com>
2018-12-28 00:49:06 +0200

    Self-ping every 5 minutes.

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const http = require(`http`);
+const express = require(`express`);
+const app = express();
+app.get(`/`, (request, response) => {
+  console.log(Date.now() + ` Ping Received`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const fs = require(`fs`);
 const { Client, Collection } = require(`discord.js`);
 const config = require(`./config.json`);
