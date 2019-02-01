417df960bb74d62ec4e659f0c86651e0702bd2e3
kometh0616 <realdr1v3n@gmail.com>
2019-02-01 16:03:27 +0200

    smol bugfixes

--- src/commands/whoknows.js
+++ src/commands/whoknows.js
@@ -1,7 +1,7 @@
 const fetch = require(`node-fetch`);
 const { stringify } = require(`querystring`);
 const { RichEmbed } = require(`discord.js`);
-const sortingFunc = (a, b) => parseInt(a.plays) - parseInt(b.plays);
+const sortingFunc = (a, b) => parseInt(b.plays) - parseInt(a.plays);
 
 
 exports.run = async (client, message, args) => {
@@ -19,8 +19,11 @@ exports.run = async (client, message, args) => {
     const query = stringify(params);
     const { artist } = await fetch(client.config.lastFM.endpoint + query)
       .then(r => r.json());
+
+    if (!artist) return message.reply(`there is no such artist as ` +
+    `\`${artistName}\` in Last.fm.`);
+
     for (const [id, member] of message.guild.members) {
-      if (id === message.member.id) continue;
       const dbParams = { where: { discordUserID: id } };
       const user = await Users.findOne(dbParams);
       if (!user) continue;
