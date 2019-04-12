42498df0521efde78698ef38200d12d54815875b
Kaleb Klein <klein.jae@gmail.com>
2019-04-12 18:01:33 -0400

    Login fixes
    Fixed issue where logged out user would cause errors with error handling
    Added fix to np command changes, so that should be resolved too
    
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- src/commands/ytnp.js
+++ src/commands/ytnp.js
@@ -1,39 +1,46 @@
 const { google } = require(`googleapis`);
 const { fetchtrack } = require(`../utils/fetchtrack`);
+const { fetchuser } = require(`../utils/fetchuser`);
 
 exports.run = async (client, message) => {
   try {
-    var ft = new fetchtrack(client, message);
-    var current = await ft.getcurrenttrack();
+    var user = new fetchuser(client, message);
 
-    if (current) {
-      var yt = google.youtube({
-        version: `v3`,
-        auth: client.config.youtube.apikey
-      });
-      yt.search.list({
-        part: `snippet`,
-        q: `${current.artist[`#text`]} ${current.name}`
-      }, function (err, result) {
-        if (err) {
-          console.error(`Error: ${err}`);
-        }
+    if (await user.get()) {
+      var ft = new fetchtrack(client, message);
+      var current = await ft.getcurrenttrack();
 
-        if (result && result.data.items.length > 0) {
-          for (var item of result.data.items) {
-            if (item.id.kind === `youtube#video`) {
-              var url = `https://youtu.be/${item.id.videoId}`;
-              message.channel.send(`${message.author.username} is listening to ${current.name} by ${current.artist[`#text`]}: ${url}`);
-
-              break;
-            }
+      if (current) {
+        var yt = google.youtube({
+          version: `v3`,
+          auth: client.config.youtube.apikey
+        });
+        yt.search.list({
+          part: `snippet`,
+          q: `${current.artist[`#text`]} ${current.name}`
+        }, function (err, result) {
+          if (err) {
+            console.error(`Error: ${err}`);
           }
-        } else {
-          message.reply(`Couldn't find a YouTube video for the track you're listening to.`);
-        }
-      });
+
+          if (result && result.data.items.length > 0) {
+            for (var item of result.data.items) {
+              if (item.id.kind === `youtube#video`) {
+                var url = `https://youtu.be/${item.id.videoId}`;
+                message.channel.send(`${message.author.username} is listening to ${current.name} by ${current.artist[`#text`]}: ${url}`);
+
+                break;
+              }
+            }
+          } else {
+            message.reply(`Couldn't find a YouTube video for the track you're listening to.`);
+          }
+        });
+      } else {
+        await message.reply(client.snippets.notPlaying);
+      }
     } else {
-      await message.reply(client.snippets.notPlaying);
+      await message.reply(client.snippets.noLogin);
     }
   } catch (e) {
     console.error(e);
