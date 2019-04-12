cd0e1309e596db4b700cd31dea9c60b3d9dfb393
kometh0616 <realdr1v3n@gmail.com>
2019-04-13 00:11:26 +0300

    Had to perform a reinstall

--- bot.js
+++ bot.js
@@ -19,7 +19,6 @@ const sequelize = new Sequelize(`database`, `user`, `password`, {
   host: `localhost`,
   dialect: `sqlite`,
   logging: false,
-  operatorsAliases: false,
   storage: `database.sqlite`
 });
 
