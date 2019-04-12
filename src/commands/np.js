2ee6877e45e6f1292f361ac54c698d4fc1ccf850
Kaleb Klein <klein.jae@gmail.com>
2019-04-12 00:50:56 -0400

    Changed np to use new util classes
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- src/commands/np.js
+++ src/commands/np.js
@@ -1,48 +1,44 @@
 const { stringify } = require(`querystring`);
 const fetch = require(`node-fetch`);
 const { RichEmbed } = require(`discord.js`);
+const { fetchuser } = require(`../utils/fetchuser`);
+const { fetchtrack } = require(`../utils/fetchtrack`);
+
 
 exports.run = async (client, message) => {
   try {
     const { apikey, endpoint } = client.config.lastFM;
-    const Users = client.sequelize.import(`../models/Users.js`);
-    const user = await Users.findOne({
-      where: {
-        discordUserID: message.author.id
-      }
-    });
-    if (!user) return message.reply(client.snippets.noLogin);
-    const lUsername = user.get(`lastFMUsername`);
-    const params = {
-      method: `user.getrecenttracks`,
-      user: lUsername,
-      api_key: apikey,
-      format: `json`
-    };
-    const query = stringify(params);
-    const data = await fetch(endpoint + query).then(r => r.json());
-    const track = data.recenttracks.track[0];
-    const prevTrack = data.recenttracks.track[1];
-    if (!track[`@attr`])
-      return message.reply(client.snippets.notPlaying);
-    const userParams = Object.assign({}, params);
-    userParams.method = `user.getinfo`;
-    const userQuery = stringify(userParams);
-    const userData = await fetch(endpoint + userQuery).then(r => r.json());
-    const embed = new RichEmbed()
-      .addField(`Current:`, `**${track.name}** - ${track.artist[`#text`]} ` +
-        `| ${track.album[`#text`] ? track.album[`#text`] : `no album`}`)
-      .addField(`Previous:`,
-        `**${prevTrack.name}** - ${prevTrack.artist[`#text`]} | ` +
-        `${prevTrack.album[`#text`] ? prevTrack.album[`#text`] : `no album`}`)
-      .setColor(message.member.displayColor)
-      .setTitle(`Last tracks from ${lUsername}`)
-      .setURL(userData.user.url)
-      .setThumbnail(track.image[2][`#text`])
-      .setFooter(`Command invoked by ${message.author.tag} with a total ` +
-        `of ${userData.user.playcount} scrobbles.`)
-      .setTimestamp();
-    await message.channel.send({ embed });
+    var user = new fetchuser(client, message);
+    var ft = new fetchtrack(client, message);
+    var track = await ft.getcurrenttrack(client, message);
+
+    if (track) {
+      var prevTrack = await ft.getlasttrack(client, message);
+      var query = stringify({
+        method: `user.getinfo`,
+        user: await user.username(),
+        api_key: apikey,
+        format: `json`
+      });
+
+      var userData = await fetch(endpoint + query).then(r => r.json());
+      const embed = new RichEmbed()
+        .addField(`Current:`, `**${track.name}** - ${track.artist[`#text`]} ` +
+          `| ${track.album[`#text`] ? track.album[`#text`] : `no album`}`)
+        .addField(`Previous:`,
+          `**${prevTrack.name}** - ${prevTrack.artist[`#text`]} | ` +
+          `${prevTrack.album[`#text`] ? prevTrack.album[`#text`] : `no album`}`)
+        .setColor(message.member.displayColor)
+        .setTitle(`Last tracks from ${await user.username()}`)
+        .setURL(userData.user.url)
+        .setThumbnail(track.image[2][`#text`])
+        .setFooter(`Command invoked by ${message.author.tag} with a total ` +
+          `of ${userData.user.playcount} scrobbles.`)
+        .setTimestamp();
+      await message.channel.send({ embed });
+    } else {
+      await message.reply(client.snippets.notPlaying);
+    }
   } catch (e) {
     console.error(e);
     await message.channel.send(client.snippets.error);
