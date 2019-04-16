6a9a3943d0295e522b382f6615fe4759d99fc93d
kometh0616 <realdr1v3n@gmail.com>
2019-04-16 19:34:01 +0300

    Some string changes

--- src/commands/lfm.js
+++ src/commands/lfm.js
@@ -8,18 +8,18 @@ exports.run = async (client, message, args) => {
     const user = await fuser.getById(discordUser.id);
 
     if (user) {
-      await message.channel.send(`**${discordUser.username}'s** LastFM URL is: https://last.fm/user/${user.get(`lastFMUsername`)}`);
+      await message.channel.send(`\`${discordUser.username}'s\` LastFM URL is: https://last.fm/user/${user.get(`lastFMUsername`)}`);
     } else {
-      await message.reply(`**${discordUser.username}** is not logged into LastFM`);
+      await message.reply(`\`${discordUser.username}\` is not logged into LastFM.`);
     }
   } else {
-    await message.reply(`${args.join(` `)} is an invalid user`);
+    await message.reply(`\`${args.join(` `)}\` is not a valid user.`);
   }
 };
 
 exports.help = {
   name: `lfm`,
-  description: `Gets the LastFM URL of you or a given user`,
+  description: `Gets the LastFM URL of you or a given user.`,
   usage: `lfm [Discord User]`,
-  notes: `The user must be logged in to LastFM with the bot`
+  notes: `The looked up user must be logged in to LastFM with the bot.`
 };
