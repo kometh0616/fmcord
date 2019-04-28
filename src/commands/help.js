8d346e91f4d4444be1216e51ffe142608f5ee107
kometh0616 <realdr1v3n@gmail.com>
2019-04-28 17:12:54 +0300

    Fixed some bugs in help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,12 +3,16 @@ exports.run = async (client, message, args) => {
   const { prefix } = client.config;
   try {
     if (args[0] === `--manual`) {
+      if (!message.guild.me.hasPermission(`ADD_REACTIONS`))
+        return message.reply(`I do not have a permission to add reactions ` +
+        `in this server.`);
       const haveHelp = client.commands.filter(x => x.help);
       const amount = haveHelp.size;
       const sorted = [...haveHelp.sort((x, y) => x.help.name - y.help.name)];
+      const color = message.member ? message.member.displayColor : 16777215;
       const updateEmbed = it => {
         const embed = new RichEmbed()
-          .setColor(message.member.displayColor)
+          .setColor(color)
           .setTitle(`Command ${sorted[it][1].help.name}`)
           .addField(`Description:`, sorted[it][1].help.description)
           .addField(`Usage:`, `${prefix}${sorted[it][1].help.usage}`)
@@ -21,7 +25,7 @@ exports.run = async (client, message, args) => {
       };
       let currentPage = 0;
       const embed = new RichEmbed()
-        .setColor(message.member.displayColor)
+        .setColor(color)
         .setTitle(`Command ${sorted[currentPage][1].help.name}`)
         .addField(`Description:`, sorted[currentPage][1].help.description)
         .addField(`Usage:`, `${prefix}${sorted[currentPage][1].help.usage}`)
@@ -85,5 +89,5 @@ exports.help = {
   `interactive manual.`,
   usage: `help [--manual]`,
   notes: `Interactive manual will only work if a bot has a permission to add ` +
-  `and manage reactions to messages.`
+  `reactions to messages.`
 };
