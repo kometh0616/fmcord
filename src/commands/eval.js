af2a79dde33b7d82ab73ebcfe2483aa15ad27b09
kometh0616 <realdr1v3n@gmail.com>
2019-05-18 11:57:02 +0300

    Asyncified eval

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,18 +1,18 @@
 const { inspect } = require(`util`);
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
   if (message.author.id !== client.config.botOwnerID) return;
   try {
     const code = args.join(` `);
     let evaled = eval(code);
     if (typeof evaled !== `string`)
       evaled = inspect(evaled);
-    message.channel.send(evaled, {
+    await message.channel.send(evaled, {
       code: `js`,
       split: true
     });
   } catch (e) {
     console.log(e);
-    message.channel.send(e, {
+    await message.channel.send(e, {
       code: `xl`,
       split: true
     });
