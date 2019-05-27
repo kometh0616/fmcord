81c819382f28bd6c4c7b9c1f2c0138a115297752
kometh0616 <kometh0616@localhost.localdomain>
2019-05-27 21:38:15 +0300

    Improved botinfo

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -1,19 +1,30 @@
 const { RichEmbed } = require(`discord.js`);
 
 exports.run = async (client, message) => {
-  const dev = client.users.get(client.config.botOwnerID);
-  const shared = client.guilds.filter(x => x.members.has(message.author.id));
-  const color = message.member ? message.member.displayColor : 16777215;
-  const embed = new RichEmbed()
-    .setTitle(`FMcord information`)
-    .setThumbnail(client.user.avatarURL)
-    .addField(`Total servers:`, client.guilds.size, true)
-    .addField(`Total users:`, client.users.size, true)
-    .addField(`Used library:`, `discord.js`, true)
-    .addField(`Developed by:`, `${dev.tag} and contributors`, true)
-    .addField(`Amount of servers shared with the command invoker:`, shared.size, true)
-    .setFooter(`Command executed by ${message.author.tag}`, message.author.avatarURL)
-    .setTimestamp()
-    .setColor(color);
-  await message.channel.send({ embed });
+  try {
+    const dev = client.users.get(client.config.botOwnerID);
+    const shared = client.guilds.filter(x => x.members.has(message.author.id));
+    const color = message.member ? message.member.displayColor : 16777215;
+    const embed = new RichEmbed()
+      .setTitle(`FMcord information`)
+      .setThumbnail(client.user.avatarURL)
+      .addField(`Total servers:`, client.guilds.size, true)
+      .addField(`Total users:`, client.users.size, true)
+      .addField(`Used library:`, `discord.js`, true)
+      .addField(`Developed by:`, `${dev.tag} and contributors`, true)
+      .addField(`Amount of servers shared with the command invoker:`, shared.size, true)
+      .setFooter(`Command executed by ${message.author.tag}`, message.author.avatarURL)
+      .setTimestamp()
+      .setColor(color);
+    await message.channel.send({ embed });
+  } catch (e) {
+    console.error(e);
+    await message.channel.send(client.snippets.error);
+  }
+};
+
+exports.help = {
+  name: `botinfo`,
+  description: `Shows general information about FMcord.`,
+  usage: `botinfo`
 };
