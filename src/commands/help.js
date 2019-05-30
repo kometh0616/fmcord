1a8d762425840e41bc5078c2799e8633285b25fc
kometh0616 <kometh0616@localhost.localdomain>
2019-05-30 15:47:22 +0300

    Redesigned help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,9 +3,9 @@ const ReactionInterface = require(`../utils/ReactionInterface`);
 exports.run = async (client, message, args) => {
   const { prefix } = client.config;
   const commandsWithHelp = client.commands.filter(x => x.help);
+  const color = message.member ? message.member.displayColor : 16777215;
   try {
     if (args[0] === `--manual`) {
-      const color = message.member ? message.member.displayColor : 16777215;
       let pages = 0;
       const embeds = commandsWithHelp
         .map(x => {
@@ -37,11 +37,37 @@ exports.run = async (client, message, args) => {
         }
       });
       await rl.setKey(client.snippets.exit, rl.destroy);
+    } else if (args.length === 0) {
+      const commands = commandsWithHelp
+        .map(x => `\`${x.help.name}\``)
+        .join(`, `);
+      const embed = new RichEmbed()
+        .setTitle(`FMcord's commands`)
+        .setThumbnail(client.user.avatarURL)
+        .setColor(color)
+        .setDescription(`Do \`${prefix}help <command name>\` to get more ` +
+        `information on a command!`)
+        .addField(`Available commands:`, commands)
+        .setFooter(`Command executed by ${message.author.tag}`, message.author.avatarURL)
+        .setTimestamp();
+      await message.channel.send({ embed });
     } else {
-      const helpMessage = commandsWithHelp
-        .map(x => `**${prefix}${x.help.name}** - ${x.help.description}`)
-        .join(`\n`);
-      await message.channel.send(helpMessage);
+      const embeds = new Map();
+      commandsWithHelp.forEach(x => {
+        const embed = new RichEmbed()
+          .setColor(color)
+          .setTitle(`Command ${x.help.name}`)
+          .addField(`Description:`, x.help.description)
+          .addField(`Usage:`, `${prefix}${x.help.usage}`)
+          .setFooter(`Command executed by ${message.author.tag}`, message.author.avatarURL)
+          .setTimestamp();
+        if (x.help.notes) embed.addField(`Notes:`, x.help.notes);
+        embeds.set(x.help.name, embed);
+      });
+      const commandName = args[0];
+      const embed = embeds.get(commandName);
+      if (!embed) return message.reply(`couldn't find that command.`);
+      await message.channel.send(embed);
     }
   } catch (e) {
     console.error(e);
