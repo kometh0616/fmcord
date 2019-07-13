3ddbe5702417d61b68e2d89d35476a2afc1f781c
kometh0616 <realdr1v3n@gmail.com>
2019-07-13 12:16:12 +0300

    Removed unneeded columns from users table

--- src/models/Users.js
+++ src/models/Users.js
@@ -2,8 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define(`users`, {
     discordUserID: DataTypes.STRING,
     lastFMUsername: DataTypes.STRING,
-    lastDailyTimestamp: DataTypes.DATE,
-    dailyPlayCount: DataTypes.INTEGER,
   });
   return Users;
 };
