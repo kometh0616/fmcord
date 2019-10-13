bf2a41679e0bcd9f1a1ee183d6349e66cede5d25
kometh0616 <realdr1v3n@gmail.com>
2019-10-13 12:57:44 +0300

    Deleted everything.

--- src/models/Notifs.js
+++
@@ -1,6 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Notifs = sequelize.define(`notifs`, {
-    userID: DataTypes.STRING
-  });
-  return Notifs;
-};
--- src/models/Users.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define(`users`, {
-    discordUserID: DataTypes.STRING,
-    lastFMUsername: DataTypes.STRING,
-  });
-  return Users;
-};
