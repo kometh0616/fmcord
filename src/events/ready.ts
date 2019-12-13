73a8661360abd28ba3c159b7fd328251c419a9af
kometh0616 <realdr1v3n@gmail.com>
2019-12-13 19:41:01 +0200

    Removed the pruning on ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,15 +8,4 @@ module.exports = async (client: FMcord): Promise<void> => {
             name: `with ${total} servers | Do ${client.defaultPrefix}help!`
         }
     });
-    const script = `
-    const apology = (owner, guild) => \`Hello, \${owner}.\\n\` +
-    \`Unfortunately, FMcord had to leave your server \${guild} because it was too small (15 user accounts are needed). This was done to save some resources for the bot and make it function once again.\\n\` +
-    \`We apologise for the caused inconvenience and hope that you proceed to use our bot in bigger communities instead.\\n\` +
-    \`- The FMcord team\`
-    this.guilds.filter(x => x.members.filter(m => !m.user.bot).size < 15)
-        .forEach(guild => guild.leave()
-        .then(g => g.owner.user.createDM()
-            .then(channel => channel.send(apology(g.owner.user.username, g.name)))
-            .catch(console.error)))`;
-    await client.shard.broadcastEval(script);
 };
