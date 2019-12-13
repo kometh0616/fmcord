ca0378b956bc0540d62162d14ae9cec6541de6cf
kometh0616 <realdr1v3n@gmail.com>
2019-12-13 16:28:52 +0200

    Added 15 people limit

--- src/handler/FMcord.ts
+++ src/handler/FMcord.ts
@@ -117,7 +117,6 @@ export default class FMcord extends Client {
             response.sendStatus(200);
         });
 
-        app.listen(process.env.PORT);
         setInterval(() => {
             http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
         }, 280000);
