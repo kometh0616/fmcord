a536af88bbaa2d63ee6c81b4f94191961e8d059e
kometh0616 <realdr1v3n@gmail.com>
2020-05-09 09:53:50 +0300

    why am I like this

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -3,6 +3,6 @@ import FMcord from "../handler/FMcord";
 export default (client: FMcord): void => {
     client.editStatus(`online`, {
         type: 0,
-        name: `#ProjectBlurple servers | Do ${client.prefix}help!`
+        name: `#ProjectBlurple | Do ${client.prefix}help!`
     });
 };
