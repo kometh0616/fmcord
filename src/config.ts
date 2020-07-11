0072fabd4c319ce58fbc9e518f6699308f88bfc8
kometh0616 <realdr1v3n@gmail.com>
2020-07-11 13:14:46 +0300

    Added @types/dotenv

--- src/config.ts
+++ src/config.ts
@@ -2,17 +2,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    prefix: process.env.BOT_PREFIX || '&',
+    prefix: process.env.BOT_PREFIX ?? `&`,
     lastFM: {
-        apikey: process.env.LASTFM_API_KEY || '',
+        apikey: process.env.LASTFM_API_KEY ?? ``,
     },
     youtube: {
-        apikey: process.env.YOUTUBE_API_KEY || '',
+        apikey: process.env.YOUTUBE_API_KEY ?? ``,
     },
     spotify: {
-        id: process.env.SPOTIFY_API_KEY || '',
-        secret: process.env.SPOTIFY_API_SECRET || '',
+        id: process.env.SPOTIFY_API_KEY ?? ``,
+        secret: process.env.SPOTIFY_API_SECRET ?? ``,
     },
-    ownerID: process.env.BOT_OWNER_ID || '',
-    token: process.env.DISCORD_API_TOKEN || '',
+    ownerID: process.env.BOT_OWNER_ID ?? ``,
+    token: process.env.DISCORD_API_TOKEN ?? ``
 };
